ee505d749f79c0bb9f66f993fb6b1b44d6724a06
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-07-25 17:19:26 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    return "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    character_name = self.characters.collect{|i| i.name}
+    show_names = self.shows.collect{|i| i.name}
+    roles << "#{character_name.first} - #{show_names.first}"
+    roles
+  end
 end
